1d3f1e88063eb509a3ece59dc857343231c3f5c8
B1NGOT <shafranskiy16@gmail.com>
2025-02-26 18:07:14 +0300

    packages was renamed

--- src/manager/TaskManager.java
+++ src/manager/TaskManager.java
@@ -1,9 +1,9 @@
-package Manager;
+package manager;
 
-import Model.Epic;
-import Model.Status;
-import Model.Subtask;
-import Model.Task;
+import model.Epic;
+import model.Status;
+import model.Subtask;
+import model.Task;
 
 import java.util.ArrayList;
 import java.util.HashMap;
